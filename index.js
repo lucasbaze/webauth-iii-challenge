17d3e1c4e4f91ce3d207e3c160b4628480d69882
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-15 10:56:05 -0500

    initialized server

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+//
+//Middleware Imports
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+//
+//Initialize Server
+const server = express();
+
+//Initialize Middleware
+server.use(helmet());
+server.use(cors());
+server.use(morgan('tiny'));
+
+//
+//Dummy Route
+server.get('/', (req, res) => {
+    res.send('Server is listening. Server is watching');
+});
+
+//
+//Error Handler
+server.use((err, req, res, next) => {
+    let { code, message } = err;
+    res.status(code).json({ message });
+});
+
+//
+//Listen
+const PORT = process.env.PORT;
+server.listen(PORT, () =>
+    console.log(`Server is always listening... on ${PORT}`)
+);
