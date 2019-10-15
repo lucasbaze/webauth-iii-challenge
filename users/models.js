a97b2ff4f11ee4f7825dfe08a06fb4e7cf4002e4
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-15 12:25:14 -0500

    register routes

--- users/models.js
+++ users/models.js
@@ -10,6 +10,46 @@ const getUsers = () => {
     return db('users');
 };
 
+//
+//Create new user
+const register = newUser => {
+    let { password } = newUser;
+
+    password = b.hashSync(password, 12);
+
+    newUser.password = password;
+
+    return db('users').insert({
+        ...newUser,
+    });
+};
+
+//
+//login user
+const login = async user => {
+    let { username, password } = user;
+
+    let hash = await db
+        .select('password')
+        .from('users')
+        .where({ username })
+        .first();
+
+    if (!hash) {
+        return Promise.resolve(false);
+    }
+
+    let match = b.compareSync(password, hash.password);
+
+    if (match) {
+        return Promise.resolve(true);
+    }
+
+    return Promise.resolve(false);
+};
+
 module.exports = {
     getUsers,
+    register,
+    login,
 };
--- users/routes.js
+++ users/routes.js
@@ -3,9 +3,54 @@ const User = require('./models');
 
 //
 //Get Users
-router.get('/users', async (req, res) => {
-    let users = await User.getUsers();
-    res.status(200).json({ users });
+router.get('/users', async (req, res, next) => {
+    try {
+        let users = await User.getUsers();
+        res.status(200).json({ users });
+    } catch (e) {
+        next({
+            code: 500,
+            message: 'Server error. Gremlins attacked. Try again',
+        });
+    }
 });
+
+//
+//Create New User
+router.post('/register', registerValid, async (req, res, next) => {
+    let user = req.body;
+
+    try {
+        let newUser = await User.register(user);
+        res.status(200).json({ newUser });
+    } catch (e) {
+        next({
+            code: 500,
+            message: 'Server error. The fire nation attacked. Try again',
+        });
+    }
+});
+
+//
+//Middleware
+function registerValid(req, res, next) {
+    let user = req.body;
+
+    if (!user || !user.username || !user.password || !user.department) {
+        next({
+            code: 409,
+            message: 'Missing parameters. You done messed up',
+        });
+    }
+
+    if (user.password.length < 8) {
+        next({
+            code: 409,
+            message: 'Password not long enough. Thats what she said',
+        });
+    }
+
+    next();
+}
 
 module.exports = router;
